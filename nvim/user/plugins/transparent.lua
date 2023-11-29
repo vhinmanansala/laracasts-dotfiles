66f07d34fe90b3785c268830b94aeae5cbde5ea1
Alvin Manansala <aomanansala@gmail.com>
2023-11-29 22:10:52 +1100

    wip

---
+++ nvim/user/plugins/transparent.lua
@@ -0,0 +1,15 @@
+return {
+    "xiyaowong/transparent.nvim",
+    config = function()
+        require("transparent").setup(
+            {
+                extra_groups = {
+                    "NvimTreeNormal",
+                    "NvimTreeNormalNC",
+                    "NvimTreeNormalFloat"
+                }
+            }
+        )
+        vim.cmd("TransparentEnable")
+    end
+}
